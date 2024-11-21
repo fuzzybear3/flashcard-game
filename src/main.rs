29b7761b890d0c9a86b3fa17c4bd77a4d49f0fb1
Steven Guido <stevenrguido@gmail.com>
2024-11-21 13:50:23 +0100

    start rework sign spawner

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ struct Person;
 #[derive(Component)]
 struct Sign {
     image_handle: Handle<Image>,
+    ui_id: Entity,
 }
 
 #[derive(Component)]
@@ -104,6 +105,8 @@ fn main() {
                 sign_spawn_manager,
                 close_on_esc,
                 resource_debug_system,
+                // print_parent_child_pairs,
+                // print_parent_child_pairs_with_components,
             ),
         )
         .run();
@@ -318,10 +321,9 @@ fn sign_spawn_manager(
     let mut removed_sign = false;
     for (entity, transform, sign) in &signs_query {
         if distance_traveled - transform.translation.x > 10. {
-            // commands.entity(entity).clear();
-
-            images.remove(sign.image_handle.id());
+            // commands.entity(entity).clear()            images.remove(sign.image_handle.id());
             commands.entity(entity).despawn_recursive();
+            commands.entity(sign.ui_id).despawn_recursive();
             removed_sign = true;
         }
     }
@@ -336,7 +338,118 @@ fn sign_spawn_manager(
             vocabulary.ramdom_translation(),
             distance_traveled + SIGN_SPACING_DISTANCE * NUMBER_OF_SIGNS as f32,
         );
+        println!("spawn signs");
     }
+}
+
+fn create_sign(
+    commands: &mut Commands,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+    images: &mut ResMut<Assets<Image>>,
+    text_content: &str,
+    transform: Transform,
+    meshes: &mut ResMut<Assets<Mesh>>,
+) -> Entity {
+    let size = Extent3d {
+        width: 512,
+        height: 512,
+        ..default()
+    };
+
+    let mut image = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Bgra8UnormSrgb,
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..default()
+    };
+    // Fill image data with zeroes
+    image.resize(size);
+
+    let image_handle = images.add(image);
+
+    // Create a unique camera for rendering each texture with different text
+    let texture_camera = commands
+        .spawn(Camera2dBundle {
+            camera: Camera {
+                order: -1,
+                target: RenderTarget::Image(image_handle.clone()),
+                ..default()
+            },
+            ..default()
+        })
+        .id();
+
+    // Set up the UI text for the texture
+    let ui = commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.),
+                    height: Val::Percent(100.),
+                    flex_direction: FlexDirection::Column,
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                background_color: GOLD.into(),
+                ..default()
+            },
+            TargetCamera(texture_camera),
+        ))
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
+                text_content,
+                TextStyle {
+                    font_size: 80.0,
+                    color: Color::BLACK,
+                    ..default()
+                },
+            ));
+        })
+        .id();
+
+    // This material has the texture that has been rendered.
+    let material_handle = materials.add(StandardMaterial {
+        base_color_texture: Some(image_handle.clone()),
+        reflectance: 0.02,
+        unlit: false,
+        ..default()
+    });
+
+    // commands.entity(parent_entity).add_child(ui);
+    let sign_mesh = commands
+        .spawn((PbrBundle {
+            mesh: meshes.add(Cuboid::new(1.0, 6.0, 3.0)),
+            // material: materials.add(Color::srgb_u8(124, 144, 255)),
+            material: material_handle,
+            transform,
+            ..default()
+        },))
+        .id();
+
+    // commands.entity(sign_mesh).add_child(ui);
+    commands.entity(sign_mesh).insert(Sign {
+        image_handle: image_handle.clone(),
+        ui_id: ui,
+    });
+    commands.entity(sign_mesh).add_child(texture_camera);
+
+    // todo I don't know why but if I add ui as a child of sign_mesh, the signs are black.... so
+    // for a work around I am storing the id in the Sign component and despawning it manually.
+    // commands
+    //     .entity(sign_mesh)
+    //     .push_children(&[texture_camera, ui]);
+
+    sign_mesh
 }
 
 fn spawn_signs(
@@ -350,104 +463,39 @@ fn spawn_signs(
 ) {
     const SIGN_DISTANCE_FROM_CENTER: f32 = 6.;
 
-    fn create_sign_texture(
-        commands: &mut Commands,
-        images: &mut ResMut<Assets<Image>>,
-        text_content: &str,
-        parent_entity: Entity,
-    ) -> Handle<Image> {
-        let size = Extent3d {
-            width: 512,
-            height: 512,
-            ..default()
-        };
+    let transform = Transform::from_xyz(distance, 1.5, -SIGN_DISTANCE_FROM_CENTER)
+        .with_rotation(Quat::from_rotation_x(-PI / 2.) * Quat::from_rotation_z(PI / 16.));
 
-        let mut image = Image {
-            texture_descriptor: TextureDescriptor {
-                label: None,
-                size,
-                dimension: TextureDimension::D2,
-                format: TextureFormat::Bgra8UnormSrgb,
-                mip_level_count: 1,
-                sample_count: 1,
-                usage: TextureUsages::TEXTURE_BINDING
-                    | TextureUsages::COPY_DST
-                    | TextureUsages::RENDER_ATTACHMENT,
-                view_formats: &[],
-            },
-            ..default()
-        };
-
-        // Fill image data with zeroes
-        image.resize(size);
-
-        let image_handle = images.add(image);
-
-        // Create a unique camera for rendering each texture with different text
-        let texture_camera = commands
-            .spawn(Camera2dBundle {
-                camera: Camera {
-                    order: -1,
-                    target: RenderTarget::Image(image_handle.clone()),
-                    ..default()
-                },
-                ..default()
-            })
-            .id();
-        commands
-            .entity(parent_entity)
-            .push_children(&[texture_camera]);
-        // Set up the UI text for the texture
-        commands
-            .spawn((
-                NodeBundle {
-                    style: Style {
-                        width: Val::Percent(100.),
-                        height: Val::Percent(100.),
-                        flex_direction: FlexDirection::Column,
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    background_color: GOLD.into(),
-                    ..default()
-                },
-                TargetCamera(texture_camera),
-            ))
-            .with_children(|parent| {
-                parent.spawn(TextBundle::from_section(
-                    text_content,
-                    TextStyle {
-                        font_size: 80.0,
-                        color: Color::BLACK,
-                        ..default()
-                    },
-                ));
-            });
-
-        image_handle
-    }
-
-    let left_sign_entity = commands.spawn(SpatialBundle { ..default() }).id();
-    let left_image_handle = create_sign_texture(
+    let sign = create_sign(
         commands,
+        materials,
         images,
-        translation.romaji.as_str(),
-        left_sign_entity,
+        other_translation.romaji.as_str(),
+        transform,
+        meshes,
     );
-    // let right_image_handle =
-    //     create_sign_texture(commands, images, other_translation.romaji.as_str());
+
+    // let left_sign_entity = commands
+    //     .spawn((SpatialBundle { ..default() }, Name::new("left_sign")))
+    //     .id();
+
+    // let left_image_handle = create_sign_texture(
+    //     commands,
+    //     images,
+    //     translation.romaji.as_str(),
+    //     left_sign_entity,
+    // );
     //
     // let middle_image_handle =
     //     create_sign_texture(commands, images, translation.english_translation.as_str());
 
     // This material has the texture that has been rendered.
-    let left_material_handle = materials.add(StandardMaterial {
-        base_color_texture: Some(left_image_handle.clone()),
-        reflectance: 0.02,
-        unlit: false,
-        ..default()
-    });
+    // let left_material_handle = materials.add(StandardMaterial {
+    //     base_color_texture: Some(left_image_handle.clone()),
+    //     reflectance: 0.02,
+    //     unlit: false,
+    //     ..default()
+    // });
 
     // let right_material_handle = materials.add(StandardMaterial {
     //     base_color_texture: Some(right_image_handle),
@@ -467,27 +515,28 @@ fn spawn_signs(
 
     // Spawn a cube mesh that is scaled to be flat along one axis
     // Left sign
-    let left_sign_mesh = commands
-        .spawn((
-            PbrBundle {
-                mesh: meshes.add(Cuboid::new(1.0, 6.0, 3.0)),
-                // material: materials.add(Color::srgb_u8(124, 144, 255)),
-                material: left_material_handle,
-                transform: Transform::from_xyz(distance, 1.5, -SIGN_DISTANCE_FROM_CENTER)
-                    .with_rotation(
-                        Quat::from_rotation_x(-PI / 2.) * Quat::from_rotation_z(PI / 16.),
-                    ),
-                ..default()
-            },
-            Sign {
-                image_handle: left_image_handle,
-            },
-        ))
-        .id();
-
-    commands
-        .entity(left_sign_entity)
-        .push_children(&[left_sign_mesh]);
+    // let left_sign_mesh = commands
+    //     .spawn((
+    //         PbrBundle {
+    //             mesh: meshes.add(Cuboid::new(1.0, 6.0, 3.0)),
+    //             // material: materials.add(Color::srgb_u8(124, 144, 255)),
+    //             material: left_material_handle,
+    //             transform: Transform::from_xyz(distance, 1.5, -SIGN_DISTANCE_FROM_CENTER)
+    //                 .with_rotation(
+    //                     Quat::from_rotation_x(-PI / 2.) * Quat::from_rotation_z(PI / 16.),
+    //                 ),
+    //             ..default()
+    //         },
+    //         Sign {
+    //             image_handle: left_image_handle.clone(),
+    //         },
+    //     ))
+    //     .id();
+    //
+    // commands.entity(left_sign_entity).add_child(left_sign_mesh);
+    // commands.entity(left_sign_entity).insert(Sign {
+    //     image_handle: left_image_handle,
+    // });
     // Right sign
     // commands.spawn((
     //     PbrBundle {
@@ -526,7 +575,7 @@ pub fn close_on_esc(
         }
 
         if input.just_pressed(KeyCode::Escape) {
-            commands.entity(window).despawn_recursive();
+            commands.entity(window).despawn();
         }
     }
 }
@@ -571,5 +620,86 @@ fn resource_debug_system(
         println!("Number of Meshes: {}", num_meshes);
         println!("Number of Cameras: {}", num_cameras);
         println!("=========================");
+    }
+}
+
+fn print_parent_child_pairs(
+    parent_query: Query<(Entity, &Children)>,
+    child_query: Query<(Entity, &Parent)>,
+    time: Res<Time>,
+    mut timer: Local<Timer>,
+) {
+    // Initialize the timer on first run
+    if timer.finished() {
+        timer.set_duration(std::time::Duration::from_secs(3));
+        timer.reset();
+    }
+    // Set up a timer to print this information every 3 seconds
+    if timer.tick(time.delta()).finished() {
+        // Print all parent -> child relationships
+        for (parent, children) in parent_query.iter() {
+            for &child in children.iter() {
+                println!("Parent: {:?} -> Child: {:?}", parent, child);
+            }
+        }
+
+        // // Print all child -> parent relationships
+        // for (child, parent) in child_query.iter() {
+        //     println!("Child: {:?} -> Parent: {:?}", child, parent.get());
+        // }
+        println!("-----------------------------------------------------");
+    }
+}
+
+// Define a custom trait for easier component printing
+trait DebuggableComponent {
+    fn as_debug_string(&self) -> String;
+}
+
+// Implement the trait for standard components
+impl DebuggableComponent for Name {
+    fn as_debug_string(&self) -> String {
+        format!("Name: {}", self.as_str())
+    }
+}
+
+// Add more implementations as needed for other components (Color, Transform, etc.)
+
+// Debug system to print parent-child pairs and their components
+fn print_parent_child_pairs_with_components(
+    parent_query: Query<(Entity, &Children, Option<&Name>)>,
+    child_query: Query<(Entity, &Parent, Option<&Name>)>,
+    time: Res<Time>,
+    mut timer: Local<Timer>,
+) {
+    // Initialize the timer on first run
+    if timer.finished() {
+        timer.set_duration(std::time::Duration::from_secs(3));
+        timer.reset();
+    }
+    // Set up a timer to print this information every 3 seconds
+    if timer.tick(time.delta()).finished() {
+        for (parent, children, parent_name) in parent_query.iter() {
+            let parent_info = match parent_name {
+                Some(name) => name.as_debug_string(),
+                None => "Unnamed Parent".to_string(),
+            };
+
+            println!("Parent: {:?} ({})", parent, parent_info);
+
+            for &child in children.iter() {
+                // Fetch child component if available
+                if let Ok((_, _, Some(child_name))) = child_query.get(child) {
+                    println!(
+                        "    -> Child: {:?} (Name: {})",
+                        child,
+                        child_name.as_debug_string()
+                    );
+                } else {
+                    println!("    -> Child: {:?} (Unnamed Child)", child);
+                }
+            }
+        }
+        println!("-----------------------------------------------------");
     }
 }
